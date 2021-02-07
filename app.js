728fc931af7510df7622add1293f12012a2aee27
Omer Melih Gul <omermelihgul@hotmail.com>
2021-02-07 03:30:05 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.static("public"));
+// app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+
+mongoose.connect("mongodb+srv://admin:simplelist135@cluster0.0wtpw.mongodb.net/SL?retryWrites=true&w=majority", {useUnifiedTopology: true} )
+
+const userSchema = {
+	name: String,
+	iotid: String,
+	email: String,
+	password: String
+};
+
+const User = new mongoose.model("users", userSchema);
+
+app.get("/", function (req, res){
+	res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/login", function (req, res){
+	res.sendFile(__dirname + "/login.html");
+});
+
+app.get("/register", function (req, res){
+	res.sendFile(__dirname + "/views/register.html");
+});
+
+app.post("/register", function (req, res){
+	const newUser = new User({
+		name: req.body.name,
+		iotid: req.body.iotid,
+		email: req.body.username,
+		password: req.body.password
+	});
+	newUser.save(function(err){
+		if(err){
+			console.log(err)
+		}
+		else{
+			res.send();
+		}
+	});
+});
+
+app.listen(3000, function(){
+	console.log("Server started on port 3000.")
+});
